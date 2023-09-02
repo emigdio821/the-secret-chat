236937af18b174df69f97c9b21dc4ce7bdad385e
Emigdio Torres <emigdio821@gmail.com>
2023-09-01 21:02:43 -0600

    feat: github sign in for prod

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,99 +1,17 @@
-// import {
-//   Box,
-//   Text,
-//   Stack,
-//   Center,
-//   IconButton,
-//   useColorMode,
-//   useColorModeValue,
-// } from '@chakra-ui/react'
-// import { useState } from 'react'
-// import LoginLogo from 'components/LoginLogo'
-// import { signIn } from 'next-auth/react'
-// import { BiMoon, BiSun } from 'react-icons/bi'
-// import { FaGithub } from 'react-icons/fa'
-
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card'
 import { LoginForm } from '@/components/login/form'
+import { SignInOptions } from '@/components/login/signin-options'
 
-// import useBgGradient from 'hooks/useBgGradient'
-// import Helmet from 'components/Helmet'
-// import CommonBtn from 'components/CommonBtn'
+const isProd = process.env.NODE_ENV === 'production'
 
 export default function LoginPage() {
-  // const bgGradient = useBgGradient()
-  // const { toggleColorMode } = useColorMode()
-  // const [isLoading, setLoading] = useState<boolean>(false)
-  // const btnBg = useColorModeValue('#444', '#262626')
-  // const btnHover = useColorModeValue('#333', '#222')
-  // const SwitchIcon = useColorModeValue(BiMoon, BiSun)
-
-  // async function handleSignIn() {
-  //   setLoading(true)
-  //   try {
-  //     await signIn('github', {
-  //       callbackUrl: '/',
-  //     })
-  //   } catch (error) {
-  //     console.log(error)
-  //     setLoading(false)
-  //   }
-  // }
-
   return (
     <Card className="mx-auto max-w-sm">
       <CardHeader>
         <CardTitle>Login</CardTitle>
         <CardDescription>Start chatting now</CardDescription>
       </CardHeader>
-      <CardContent>
-        <LoginForm />
-      </CardContent>
+      <CardContent>{isProd ? <SignInOptions /> : <LoginForm />}</CardContent>
     </Card>
-    // <Center p={6} minH="100vh" bgImage={bgGradient}>
-    //   <Helmet title="Login" />
-    //   <Box
-    //     w="md"
-    //     shadow="xl"
-    //     rounded="lg"
-    //     overflow="hidden"
-    //     bg={useColorModeValue('#EDEDED', '#202020')}
-    //   >
-    //     <Box p={6}>
-    //       <Stack spacing={0} align="center" mb={5}>
-    //         <LoginLogo />
-    //         <Text fontSize="xl" fontWeight={600}>
-    //           Start chatting now
-    //         </Text>
-    //       </Stack>
-    //       <Stack justifyContent="center" direction="row">
-    //         <CommonBtn
-    //           isLoading={isLoading}
-    //           onClick={() => handleSignIn()}
-    //           btnLabel="Log in with Github"
-    //           leftIcon={<FaGithub size={20} />}
-    //         />
-    //         <IconButton
-    //           bg={btnBg}
-    //           p={[0, 4]}
-    //           rounded="full"
-    //           onClick={toggleColorMode}
-    //           aria-label="Switch theme"
-    //           icon={<SwitchIcon size={20} />}
-    //           color={useColorModeValue('yellow.400', 'purple.200')}
-    //           _hover={{
-    //             bg: btnHover,
-    //           }}
-    //           _active={{
-    //             bg: btnHover,
-    //           }}
-    //         />
-    //         <button className="text-white px-4 sm:px-8 py-2 sm:py-3 bg-sky-700 hover:bg-sky-800">
-    //           ...
-    //         </button>
-    //       </Stack>
-    //     </Box>
-    //   </Box>
-    // </Center>
   )
 }
---
+++ components/login/signin-options.tsx
@@ -0,0 +1,40 @@
+'use client'
+
+import { useToggle } from '@uidotdev/usehooks'
+import { Github } from 'lucide-react'
+import { signIn } from 'next-auth/react'
+import { toast } from 'sonner'
+
+import { Button } from '@/components/ui/button'
+import { Loader } from '@/components/icons'
+
+export function SignInOptions() {
+  const [isLoading, setLoading] = useToggle()
+
+  async function handleSignIn() {
+    try {
+      setLoading(true)
+      await signIn('github', {
+        callbackUrl: '/',
+      })
+    } catch (err) {
+      setLoading(false)
+      const errMessage = err instanceof Error ? err.message : err
+      console.log('[LOGIN]', errMessage)
+      toast.error('Uh oh!', {
+        description: 'Something went wrong while signing in, try again',
+      })
+    }
+  }
+
+  return (
+    <Button variant="outline" disabled={isLoading} onClick={handleSignIn}>
+      Continue with Github
+      {isLoading ? (
+        <Loader className="ml-2" barsClassName="bg-primary" />
+      ) : (
+        <Github className="ml-2 h-4 w-4" />
+      )}
+    </Button>
+  )
+}
