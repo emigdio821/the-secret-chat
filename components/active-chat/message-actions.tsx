155d33060eef7e42e3b2d81257978e166fe44d0a
Emigdio Torres <emigdio821@gmail.com>
2025-07-15 15:24:46 -0600

    fix: msg actions emoji picker

--- components/active-chat/message-actions.tsx
+++ components/active-chat/message-actions.tsx
@@ -1,7 +1,7 @@
 import { zodResolver } from '@hookform/resolvers/zod'
 import { useToggle } from '@mantine/hooks'
 import type { Message } from '@twilio/conversations'
-import { Edit2, MoreVertical, Save, SmilePlus, Trash2 } from 'lucide-react'
+import { Edit2, MoreVertical, Save, SmilePlusIcon, Trash2 } from 'lucide-react'
 import { useForm } from 'react-hook-form'
 import { toast } from 'sonner'
 import type * as z from 'zod'
@@ -118,21 +118,15 @@ export function MessageActions({ message, editMode }: MessageActionsProps) {
                   <div className="flex items-center justify-end gap-2">
                     <EmojiPicker
                       trigger={
-                        <Button variant="secondary">
-                          <SmilePlus className="size-4" />
+                        <Button size="icon" variant="ghost" aria-label="Emoji picker">
+                          <SmilePlusIcon className="size-4" />
                         </Button>
                       }
-                      callback={(emoji) => {
+                      callback={(value) => {
                         const body = form.getValues('body')
-                        if (body) {
-                          form.setValue('body', `${body}${emoji.native}`, {
-                            shouldValidate: true,
-                          })
-                        } else {
-                          form.setValue('body', emoji.native, {
-                            shouldValidate: true,
-                          })
-                        }
+                        form.setValue('body', `${body}${value.emoji}`, {
+                          shouldValidate: true,
+                        })
                       }}
                     />
                     <Button
